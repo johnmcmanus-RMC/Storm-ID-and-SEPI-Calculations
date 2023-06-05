89557e65398df60ca007530d8435b634b246010b
John McManus <johnmcmanus@rmc.edu>
2023-06-05 09:31:12 -0400

    Update Multi Station Annual Surge Code V3.py

--- Multi Station Annual Surge Code V3.py
+++ Multi Station Annual Surge Code V3.py
@@ -1,5 +1,8 @@
 # @author: John McManus
+# @author: Michael Fenster
+# @author: Rachele Dominguez
 # @date 03/11/2023
+
 # © 2023. This work is licensed under a CC BY-NC-SA 4.0 license
 # Program to compute Storms and Annual SEPI
 
